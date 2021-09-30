65dc1dcfadd4cce123169614495d4e9fa3eba0d9
Erik <hombledal@chapman.edu>
2021-09-30 12:10:26 -0700

    bugfixes. Should be final version!

--- Assignment1/Part2/Calculator.hs
+++ Assignment1/Part2/Calculator.hs
@@ -17,5 +17,5 @@ calc s =
   in printTree (eval e)
 
 -- ghc Calculator.hs
--- echo "1+2*3/4" | ./Calculator
+-- echo "1+2*3" | ./Calculator
 
--- Assignment1/Part2/Interpreter.hs
+++ Assignment1/Part2/Interpreter.hs
@@ -8,8 +8,8 @@ eval (Plus n m) = (eval n) + (eval m)
 eval (Times n m) = (eval n) * (eval m)
 --begin added functions
 eval (Sub n m) = (eval n) - (eval m)
-eval (Divide n m) = (eval n) / (eval m)
+eval (Divide n m) = quot (eval n) (eval m)
 eval (Exponent n m) = (eval n) ^ (eval m)
-eval (Modulus n m) = (eval n) % (eval m)
+eval (Modulus n m) = mod (eval n) (eval m)
 eval (Unary n) = negate (eval n)
 eval (Binary n) = (eval n) - (1);
